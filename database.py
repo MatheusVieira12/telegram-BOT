444bcf0ed1de2854993454e6e0cdbfe6df33904e
MatheusVieira12 <matheuscoimbra21@outlook.com>
2026-05-29 00:57:43 -0300

    Migrando banco para Neon PostgreSQL

--- database.py
+++ database.py
@@ -1,12 +1,21 @@
+import os
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import declarative_base, sessionmaker
-import os
 
-os.makedirs("data", exist_ok=True)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine("sqlite:///data/gastos.db")
+# fallback local, caso você rode no PC sem DATABASE_URL
+if not DATABASE_URL:
+    os.makedirs("data", exist_ok=True)
+    DATABASE_URL = "sqlite:///data/gastos.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 Base = declarative_base()
+
 
 class Gasto(Base):
     __tablename__ = "gastos"
@@ -17,12 +26,14 @@ class Gasto(Base):
     categoria = Column(String)
     data = Column(String)
 
+
 class CategoriaPersonalizada(Base):
     __tablename__ = "categorias_personalizadas"
 
     id = Column(Integer, primary_key=True)
     palavra = Column(String, unique=True)
     categoria = Column(String)
+
 
 Base.metadata.create_all(engine)
 
